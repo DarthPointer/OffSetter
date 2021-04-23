bd2cbc0276a746a35ea9507c44ed39791437d593
DarthPointer <al.kiriackin@yandex.ru>
2021-04-23 21:17:57 +0300

    Initial uploading

---
+++ Downloadables/OffSetter/ReadMe.txt
@@ -0,0 +1,49 @@
+This is an ASAP-coded .net5 application to automate offset changes in .uasset files for modding purposes.
+
+In order to make extensive edits of .uasset and .uexp files and keep them pars-able for the UE4 engine,
+you should update offsets updated according to size changes in different blocks of the files. This tool
+can handle all the offsets "offsetting" in some of the scenarios as long as you can tell it where
+and how much the size was changed.
+
+v 0.0
+
+Supported offseting cases:
+
+- Changed size of names block of .uasset file.
+- Changed structure size in .uexp file (sub-structure size marks in the .uexp should be changed manually).
+
+Instructions:
+
+- Drag'n'drop a .uasset that needs offsets updating into the .exe.
+- Input names number change.
+- Input net size change of names block.
+- Input change of a struct size in the .uexp.
+- Input the struct's serial offset.
+- You should get a copy of the original file with changed offsets with .offset "suffix".
+
+Warning:
+
+It is strongly recommended to perform "offsetting" in small steps along editing the files. It can be easy to
+get confused with numbers you need to input after performing complex operations with the files, including size changes
+in different places.
+
+"Easy and safe" step can include:
+1) Multiple changes in names block (you only need to know net changes of names count
+and the block's size).
+2) Size change of a single exported structure referred in exports block. You need to know its initial SerialOffset and
+its net size change.
+
+Try to keep all changes between "offsettings" fitting these 2 limitations of single "offsetting pass".
+
+DO NOT apply exported structure size change corrections prior to applying all changes connected with sizes of Names,
+Imports and Exprots blocks! If changes only involve exported struct and Names block size changes, then you can apply both
+corretions with single "offsetting pass". 
+
+Avoid changing sizes of 2 exported structures without automatic "offsetting". If you have changed net size of multiple
+exported structures and want to use this tool for automated offsets updating, you will need to pass the file to the tool
+multiple times (original file, then its .offset copy and so on). You will have to take into account offsets you made 
+on previous steps.
+
+It is still possible to perform all the offsettings with multiple "passes" after all changes are done. In that case
+you need understanding of .uasset strucure and some calculations to be done or have a reliable access to changed offsets
+after each "pass".
---
+++ Source/OffSetter/OffSetter/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.IO;
+
+namespace OffSetter
+{
+    class Program
+    {
+        private static Int32[] nameCountOffsets = { 41, 117 };
+        private static Int32[] fixedOffetsOffsets = { 24, 61, 69, 73, 165, 169, 189 };
+
+        private static Int32 exportCountOffset = 57;
+        private static Int32 exportOffsetOffset = 61;
+
+        private static Int32 relativeSerialOffsetOffset = 36;
+        private static Int32 relativeSerialSizeOffset = 28;
+        private static Int32 exportReferenceSize = 104;
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("The program needs one argument - filename to perform offsets \"offsetting\"");
+            }
+            else
+            {
+                Span<byte> bytes = File.ReadAllBytes(args[0]);
+
+                NamesOffsettingInteractive(bytes);
+
+                SerialOffsettingInteractive(bytes);
+
+                File.WriteAllBytes(args[0] + ".offset", bytes.ToArray());
+
+                Console.WriteLine("Done");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void NamesOffsettingInteractive(Span<byte> span)
+        {
+            Console.WriteLine("Input names count change");
+            Int32 namesCountChange = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input names length change");
+            Int32 namesLengthChange = Int32.Parse(Console.ReadLine());
+
+            NamesOffsetting(span, namesCountChange, namesLengthChange);
+        }
+
+        private static void NamesOffsetting(Span<byte> span, Int32 namesCountChange, Int32 namesLengthChange)
+        {
+            foreach (Int32 offset in nameCountOffsets)
+            {
+                AddToInt32ByOffset(span, namesCountChange, offset);
+            }
+
+            foreach (Int32 offset in fixedOffetsOffsets)
+            {
+                AddToInt32ByOffset(span, namesLengthChange, offset);
+            }
+
+            SerialOffsetting(span, namesLengthChange, 0);                           // All serial offsets will be affected
+        }
+
+        private static void SerialOffsettingInteractive(Span<byte> span)
+        {
+            Console.WriteLine("Input size change of struct in .uexp");
+            Int32 sizeChange = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input SerialOffset of struct changed size in .uexp");
+            Int32 sizeChangeOffset = Int32.Parse(Console.ReadLine());
+
+            SerialOffsetting(span, sizeChange, sizeChangeOffset);
+        }
+
+        private static void SerialOffsetting(Span<byte> span, Int32 sizeChange, Int32 sizeChangeOffset)
+        {
+            Int32 exportCount = Int32FromSpanOffset(span, exportCountOffset);
+            Int32 exportOffset = Int32FromSpanOffset(span, exportOffsetOffset);
+
+            Int32 currentReferenceOffset = exportOffset;
+            bool applyOffsetChange = false;
+            int processedReferences = 0;
+
+            while (processedReferences < exportCount)
+            {
+                if (!applyOffsetChange)
+                {
+                    RelativeSizeChangeDirection direction = CheckOffsetAffected(span, sizeChangeOffset, currentReferenceOffset);
+                    if (direction == RelativeSizeChangeDirection.here)
+                    {
+                        ApplySerialSizeChange(span, sizeChange, currentReferenceOffset);
+                        applyOffsetChange = true;
+                    }
+
+                    if (direction == RelativeSizeChangeDirection.before)
+                    {
+                        ApplySerialOffsetChange(span, sizeChange, currentReferenceOffset);
+                        applyOffsetChange = true;
+                    }
+                }
+                else
+                {
+                    ApplySerialOffsetChange(span, sizeChange, currentReferenceOffset);
+                }
+
+                currentReferenceOffset += exportReferenceSize;
+                processedReferences++;
+            }
+        }
+
+        private static Int32 Int32FromSpanOffset(Span<byte> span, Int32 offset)
+        {
+            return SpanToInt32(span.Slice(offset, 4));
+        }
+
+        private static Int32 SpanToInt32(Span<byte> span)
+        {
+            return span[0] + 256 * (span[1] + 256 * (span[2] + 256 * span[3]));
+        }
+
+        private static RelativeSizeChangeDirection CheckOffsetAffected(Span<byte> span, Int32 sizeChangeSerialOffset, Int32 referenceOffset)
+        {
+            Int32 serialOffset = Int32FromSpanOffset(span, referenceOffset + relativeSerialOffsetOffset);
+
+            if (serialOffset > sizeChangeSerialOffset) return RelativeSizeChangeDirection.before;
+            else if (serialOffset == sizeChangeSerialOffset) return RelativeSizeChangeDirection.here;
+            else /*if (serialOffset < sizeChangeSerialOffset)*/ return RelativeSizeChangeDirection.after;
+        }
+
+        private static void ApplySerialSizeChange(Span<byte> span, Int32 sizeChange, Int32 referenceOffset)
+        {
+            AddToInt32ByOffset(span, sizeChange, referenceOffset + relativeSerialSizeOffset);
+        }
+
+        private static void ApplySerialOffsetChange(Span<byte> span, Int32 sizeChange, Int32 referenceOffset)
+        {
+            AddToInt32ByOffset(span, sizeChange, referenceOffset+relativeSerialOffsetOffset);
+        }
+
+        private static void AddToInt32ByOffset(Span<byte> span, Int32 value, Int32 offset)
+        {
+            WriteInt32IntoOffset(span, Int32FromSpanOffset(span, offset) + value, offset);
+        }
+
+        private static void WriteInt32IntoOffset(Span<byte> span, Int32 value, Int32 offset)
+        {
+            Span<byte> scope = span.Slice(offset, 4);
+            scope[0] = (byte)(value % 256);
+            scope[1] = (byte)(value / 256 % 256);
+            scope[2] = (byte)(value / 256 / 256 % 256);
+            scope[3] = (byte)(value / 256 / 256 / 256);
+        }
+
+        private enum RelativeSizeChangeDirection
+        {
+            after = 0,
+            here = 1,
+            before = 2
+        }
+    }
+}
